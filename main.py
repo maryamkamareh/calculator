a9aa974c16d28a133bc0f9acd137f7eeb2c681ba
Maryam <maryamkamareh1995@gmail.com>
2024-04-04 09:41:08 +0330

    (num1, num2, operator)

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
-def calculator():
-    while True:
-        print("Options:")
-        print("Enter 'add' for addition")
-        print("Enter 'subtract' for subtraction")
-        print("Enter 'multiply' for multiplication")
-        print("Enter 'divide' for division")
-        print("Enter 'quit' to end the program")
+def calculator(num1,operator, num2):
+    def add_numbers(num1, num2):
+        return num1 + num2
 
-        choose_operation = input(": ")
+    def multiply_numbers(num1, num2):
+        return num1 * num2
 
-        if choose_operation == "quit":
-            break
-        elif choose_operation == "add":
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-            result = num1 + num2
-            print("Result: " + str(result))
-        elif choose_operation == "subtract":
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-            result = num1 - num2
-            print("Result: " + str(result))
-        elif choose_operation == "multiply":
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-            result = num1 * num2
-            print("Result: " + str(result))
-        elif choose_operation == "divide":
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-            if num2 == 0:
-                print("Error: Division by zero")
-            else:
-                result = num1 / num2
-                print("Result: " + str(result))
+    def subtract_numbers(num1, num2):
+        return num1 - num2
+
+    def divided_numbers(num1, num2):
+        return num1 / num2
+
+    if operator == '+':
+        result = add_numbers(num1, num2)
+    elif operator == '*':
+        result = multiply_numbers(num1, num2)
+    elif operator == '-':
+        result = subtract_numbers(num1, num2)
+    elif operator == '/':
+        if num2 == 0:
+            print("Error: Division by zero")
         else:
-            print("Invalid input. Please try again.")
+            result = divided_numbers(num1, num2)
+    else:
+        print("Invalid operator!")
+        exit()
+    print(f"{num1} {operator} {num2} = {result}")
 
-calculator()
+calculator(7, "+", 4)
