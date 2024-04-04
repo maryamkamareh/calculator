68e4bda4fc6bd843707b87e35da2b3e8590a782e
Maryam <maryamkamareh1995@gmail.com>
2024-04-04 12:25:39 +0330

    calculater with class 2+2

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-class Calculator:
-    def add(self, num1, num2):
-        return num1 + num2
-
-    def subtract(self, num1, num2):
-        return num1 - num2
-
-    def multiply(self, num1, num2):
-        return num1 * num2
-
-    def divide(self, num1, num2):
-        if num2 == 0:
-            return "Error: Division by zero!"
-        return num1 / num2
-
-calc = Calculator()
-result = calc.add(10, 3)
-print(result)
+class cal():
+    def __init__(self,num1, num2, operator):
+        self.num1 = num1
+        self.num2 = num2
+        self.operator = operator
+    def add (self):
+        return self.num1 + self.num2
+    def mul (self):
+        return self.num1 * self.num2
+    def sub (self):
+        return self.num1 - self.num2
+    def div (self):
+        return self.num1 / self.num2
+user_input = input("input operation")
+calculater = cal(float(user_input[0]), float(user_input[2]), user_input[1])
+if user_input[1] == "+":
+    print(calculater.add())
+elif user_input[1] == "*":
+    print(calculater.mul())
+elif user_input[1] == "-":
+    print(calculater.sub())
+elif user_input[1] == "/":
+    print(calculater.div())
