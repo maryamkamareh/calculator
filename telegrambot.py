c79b1aae2a9e221b2edc8c8ad5aadb04fd745215
maryamkamareh <104977345+maryamkamareh@users.noreply.github.com>
2024-07-21 16:27:49 +0330

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -9,7 +9,7 @@ from requests.exceptions import ReadTimeout
 # تنظیمات logging
 logging.basicConfig(level=logging.INFO)
 
-bot = telebot.TeleBot("7497653283:AAG2sOOBuraSlz7attsr4QhCCSdCNe9ciF8")
+bot = telebot.TeleBot("")
 
 # متغیرهای وضعیت
 user_states = {}
